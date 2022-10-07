5aa3f46b03836ab5bc8f8ec0e0ef2ffa52fc380a
Blaise Frederick <blaise.frederick@gmail.com>
2022-10-06 21:33:48 -0400

    Got pyqt installation working on both architectures

--- builddocker.sh
+++ builddocker.sh
@@ -12,20 +12,15 @@ IMAGE=basecontainer
 git pull
 
 # bump version
-#python revversion.py
+python revversion.py
 version=`cat VERSION | sed 's/+/ /g' | sed 's/v//g' | awk '{print $1}'`
 echo "version: $version"
 
 # run build
-docker build . -t $IMAGE \
+docker buildx build . \
+    --platform linux/amd64,linux/arm64 \
+    -t $IMAGE \
+    --tag $USERNAME/$IMAGE:latest --tag $USERNAME/$IMAGE:$version \
     --build-arg VERSION=$version \
     --build-arg BUILD_DATE=`date +"%Y%m%dT%H%M%S"` \
-    --build-arg VCS_REF=`git rev-parse HEAD`
-
-# tag it
-docker tag $IMAGE:latest $USERNAME/$IMAGE:latest
-docker tag $IMAGE:latest $USERNAME/$IMAGE:$version
-
-# push it
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+    --build-arg VCS_REF=`git rev-parse HEAD` --push
